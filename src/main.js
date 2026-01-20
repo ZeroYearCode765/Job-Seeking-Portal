ea6f1b46eaca2bd3fdd53218d699dafa69c6de39
Aadil <aadilsiddique765@gmail.com>
2026-01-20 08:02:02 +0530

    Installed CSS and boiler coding done

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function Main() {
+    return (
+        <>
+        </>
+
+    )
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <Main/>
+)
